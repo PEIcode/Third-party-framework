acd9a458d7d2b9ef428b2ea7e45262c048e7388f
Pei丶Code <aishiklpz@163.com>
2019-04-22 15:22:37 +0800

    [lpz]添加URLSession请求数据

--- Third-party-framework/ViewModel/PSSearchViewModel.swift
+++ Third-party-framework/ViewModel/PSSearchViewModel.swift
@@ -30,23 +30,38 @@ class PSSearchViewModel: NSObject {
         return man
     }()
     func fetchSearchDataList(){
-        
-        
-            let searchUrl = "https://newapi.meishi.cc/search/recipe"
-            
-            manager.request(searchUrl, method: .post, parameters: searchDict, encoding: URLEncoding.default, headers:nil ).responseJSON { (response) in
-                if let dict = response.result.value as? Dictionary<String, Any> {
-                    let json = JSON(dict)
-                    let dataDict = json["data"].dictionary
-                    let itemsArray = dataDict!["items"]?.arrayObject
+        let searchUrl = "https://newapi.meishi.cc/search/recipe"
+        //// 验证post请求
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        var request = URLRequest(url: URL(string: searchUrl)!)
+        request.httpMethod = "POST"
+        let bodyStr = "cid=\(cid)&keyword=豆腐&page=1"
+        request.httpBody = bodyStr.data(using: .utf8)
+//        request.
+//            let a = session.dataTask(with: URL(string: searchUrl)!)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if error == nil {
+                let dict = try! JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                print(dict)
+            }
+        }
+        task.resume()
+        ////
+
+        //alamofire请求数据
+        manager.request(searchUrl, method: .post, parameters: searchDict, encoding: URLEncoding.default, headers:nil ).responseJSON { (response) in
+            if let dict = response.result.value as? Dictionary<String, Any> {
+                let json = JSON(dict)
+                let dataDict = json["data"].dictionary
+                let itemsArray = dataDict!["items"]?.arrayObject
 //                    let itemsArray = json["data"]["items"].arrayObject
 //                    print(itemsArray!)
-                    self.resultItems = itemsArray!
+                self.resultItems = itemsArray!
 //                    print(self.resultItems.count)
-                    self.delegate?.fetchDataFinish()
+                self.delegate?.fetchDataFinish()
 
-                }
-                
-            }        
+            }
+
+        }
     }
 }
